cbabe504b7a5cbe3730d376820a5d40159af0aed
Jotas <jproodian721@gmail.com>
2022-03-26 21:23:18 -0500

    added photos to photolist component and filtered by category to add them to the gallery when clicked on nav bar

--- src/App.js
+++ src/App.js
@@ -11,22 +11,125 @@ function App() {
     },
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
+
+  const [photos] = useState([
+    {
+        name: 'Grocery aisle',
+        category: 'commercial',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Grocery booth',
+        category: 'commercial',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Building exterior',
+        category: 'commercial',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Restaurant table',
+        category: 'commercial',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Cafe interior',
+        category: 'commercial',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Cat green eyes',
+        category: 'portraits',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Green parrot',
+        category: 'portraits',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Yellow macaw',
+        category: 'portraits',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Pug smile',
+        category: 'portraits',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Pancakes',
+        category: 'food',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Burrito',
+        category: 'food',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Scallop pasta',
+        category: 'food',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Burger',
+        category: 'food',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Fruit bowl',
+        category: 'food',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Green river',
+        category: 'landscape',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Docks',
+        category: 'landscape',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Panoramic village by sea',
+        category: 'landscape',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Domestic landscape',
+        category: 'landscape',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+      {
+        name: 'Park bench',
+        category: 'landscape',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+      },
+    ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
       <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
+        categories={categories} 
+        setCurrentCategory={setCurrentCategory} 
         currentCategory={currentCategory}
       >
       </Nav>
       <main>
-        <Gallery></Gallery>
-        <About></About>
+        <div>
+          <Gallery
+            categories={categories}
+            setCurrentCategory={setCurrentCategory}
+            currentCategory={currentCategory}></Gallery>
+          <About></About>
+        </div>
       </main>
     </div>
   );
@@ -35,4 +138,4 @@ function App() {
 export default App;
 
 
-// thru 20.3.3; onto 20.3.4
+// about 1/4 thru 20.3.5
