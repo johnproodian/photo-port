a55c3b1375fc4bd863568e3ab5907f8cb64d65c3
Jotas <jproodian721@gmail.com>
2022-04-01 15:04:03 -0500

    added form validation and conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -14,104 +17,6 @@ function App() {
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
 
-  const [photos] = useState([
-    {
-        name: 'Grocery aisle',
-        category: 'commercial',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Grocery booth',
-        category: 'commercial',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Building exterior',
-        category: 'commercial',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Restaurant table',
-        category: 'commercial',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Cafe interior',
-        category: 'commercial',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Cat green eyes',
-        category: 'portraits',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Green parrot',
-        category: 'portraits',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Yellow macaw',
-        category: 'portraits',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Pug smile',
-        category: 'portraits',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Pancakes',
-        category: 'food',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Burrito',
-        category: 'food',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Scallop pasta',
-        category: 'food',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Burger',
-        category: 'food',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Fruit bowl',
-        category: 'food',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Green river',
-        category: 'landscape',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Docks',
-        category: 'landscape',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Panoramic village by sea',
-        category: 'landscape',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Domestic landscape',
-        category: 'landscape',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-      {
-        name: 'Park bench',
-        category: 'landscape',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
-      },
-    ]);
-
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
@@ -120,17 +25,20 @@ function App() {
         categories={categories} 
         setCurrentCategory={setCurrentCategory} 
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       >
       </Nav>
-      <main>
-        <div>
-          <Gallery
-            categories={categories}
-            setCurrentCategory={setCurrentCategory}
-            currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
-      </main>
+        <main>
+          {!contactSelected ? (
+            <>
+              <Gallery currentCategory={currentCategory}></Gallery>
+              <About></About>
+            </>
+          ) : (
+          <ContactForm></ContactForm>
+        )} 
+        </main>
     </div>
   );
 }
@@ -138,4 +46,4 @@ function App() {
 export default App;
 
 
-// about 1/4 thru 20.3.5
+// about 1/2 thru 20.4.6
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,9 +4,11 @@ import Gallery from "../Gallery";
 
 function Nav(props) {
     const {
-        categories,
+        categories = [],
         setCurrentCategory,
         currentCategory,
+        contactSelected,
+        setContactSelected
     } = props;
 
     useEffect(() => {
@@ -23,26 +25,28 @@ function Nav(props) {
             </h2>
             <nav>
                 <ul className="flex-row">
-                    <li className="mx-2">
+                    <li className={`mx-2`}>
                         <a data-testid="about"
                             href="#about"
+                            onClick={() => setContactSelected(false)}
                         >
                             About me
                         </a>
                     </li>
-                    <li>
-                        <span>Contact</span>
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                        <span onClick={() => setContactSelected(true)}>Contact</span>
                     </li>
                     {categories.map((category) => (
                         <li 
                             className={`mx-1 ${
-                                currentCategory.name === category.name && 'navActive'
+                                currentCategory.name === category.name && !contactSelected && 'navActive'
                                 }`} 
                             key={category.name}
                         >
                             <span 
                                 onClick={() => {
-                                    setCurrentCategory(category)
+                                    setCurrentCategory(category);
+                                    setContactSelected(false);
                                 }}
                             >
                                 {capitalizeFirstLetter(category.name)}
